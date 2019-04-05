a189ec869de70b0ba6dd93e6b92b8e51e60ae1b6
Hwang-chang-hoon <ghkd5078@gmail.com>
2019-04-05 16:55:49 +0900

    testing for master

---
+++ codetesting/src/codetesting/CodeTesting.java
@@ -0,0 +1,19 @@
+package codetesting;
+
+public class CodeTesting {
+
+	public static void main(String[] args) {
+		int intValue1 = 10;
+		int intValue2 = 20;
+		int intvalue3 = intValue1 + intValue2;
+		System.out.println(intvalue3);
+		
+		
+		char charValue1 = 'A';
+		char charValue2 = 1;
+		int intValue4 = charValue1 + charValue2;
+		System.out.println("유니코드=" + intValue4);
+
+	}
+
+}
